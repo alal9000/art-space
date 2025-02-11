93f17ddd3cc2f7602cd45d087d5d9357cc157381
aaron <lucidlear@gmail.com>
2025-02-11 12:09:11 +1100

    static screen built out

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.artspace"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.artspace"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,13 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +39,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtSpaceTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    ArtSpaceLayout()
                 }
             }
         }
@@ -31,17 +50,87 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ArtSpaceLayout() {
+    Column {
+        ArtWorkCanvas()
+        ArtWorkDescription()
+        DisplayController(
+            modifier = Modifier.padding(top = 32.dp)
+        )
+    }
+}
+
+@Composable
+fun ArtWorkCanvas() {
+
+
+    Image(
+        painter = painterResource(id = R.drawable._01_digitalart),
+        contentDescription = "Statue with Halo",
+        modifier = Modifier
+            .size(500.dp, 600.dp)  // Set a fixed size to prevent taking up all available space
+            .padding(16.dp),
+        contentScale = ContentScale.Fit
+        )
+}
+
+@Composable
+fun ArtWorkDescription() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+
+    ) {
+        Text(
+            text = stringResource(id = R.string.artwork_1_title),
+            color = Color.DarkGray,
+            fontSize = 28.sp
+        )
+        Text(
+            stringResource(id = R.string.artist_1),
+            fontWeight = FontWeight.Thin,
+            fontStyle = FontStyle.Italic
+
+        )
+        Text(
+            stringResource(id = R.string.artwork_1_year),
+            fontWeight = FontWeight.Thin,
+            fontStyle = FontStyle.Italic
+        )
+
+    }
+}
+
+@Composable
+fun DisplayController(
+    modifier: Modifier = Modifier
+
+) {
+    Row(
         modifier = modifier
-    )
+            .fillMaxWidth()
+            .padding(top = 8.dp),
+        horizontalArrangement = Arrangement.Center,
+
+    ) {
+        Button(onClick = { /* handle previous button click */}) {
+            Text(text = "Previous")
+        }
+
+        Button(
+            onClick = { /* handle next button click */},
+            modifier = Modifier.padding(start = 8.dp)
+            ) {
+            Text(text = "Next")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtWorkImagePreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceLayout()
     }
 }
